2b11b02c84f0d04813ea06eebc622bfb2d30af48
satoshi toyama <satoshi.toyama@route06.co.jp>
2026-01-22 17:03:09 +0900

    debug

--- packages/web/src/lib/auth-client.ts
+++ packages/web/src/lib/auth-client.ts
@@ -2,6 +2,8 @@ import { polarClient } from "@polar-sh/better-auth";
 import { deviceAuthorizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
+console.log(`process.env.NEXT_PUBLIC_APP_URL:${process.env.NEXT_PUBLIC_APP_URL}`)
+
 const authClient = createAuthClient({
 	baseURL: process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
 	plugins: [deviceAuthorizationClient(), polarClient()],
